d98637b4b0452d9a7a144c6b8a08db1153aec7fc
Nick Meinhold <nick.meinhold@gmail.com>
2019-10-04 16:46:21 +1000

    Regenerate API Key accidentally added to github
    - regenerated maps api key
    - added cocoapods-keys plugin to podfile with GoogleMapsApi entry
    - changed AppDelegate to use key stored with cocoapods-keys

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import GoogleMaps
+import Keys
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +9,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyABm3d6zgMnC9-9hvXUfNkV7tWhEXZoHHQ")
+    GMSServices.provideAPIKey(RunnerKeys().googleMapsApi)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
